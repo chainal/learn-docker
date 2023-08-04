56371be8e2c0bc7017af8fbb9b657fa2e68f33be
liuchuang <liuchuang@cmhi.chinamobile.com>
2023-08-04 23:51:22 +0800

    add java17 app

---
+++ app-java/src/test/java/com/hinako/learn/kotoba/KotobaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hinako.learn.kotoba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotobaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
