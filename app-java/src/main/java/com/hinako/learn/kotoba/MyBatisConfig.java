e5f9197c8afc61377821156cddf9f2cea642ace9
chainal <chainal@qq.com>
2023-08-08 20:24:50 +0800

    add connectj datasource batis

--- app-java/src/main/java/com/hinako/learn/kotoba/MyBatisConfig.java
+++ app-java/src/main/java/com/hinako/learn/kotoba/MyBatisConfig.java
@@ -1,5 +1,20 @@
 package com.hinako.learn.kotoba;
 
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class MyBatisConfig {
+
+    DriverManagerDataSource buildDriverManagerDataSource() {
+
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource());
+        return factoryBean.getObject();
+    }
 }
