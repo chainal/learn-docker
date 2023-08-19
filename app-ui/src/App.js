7b55a4eefd7b23f683219b7613f041bd339e6c90
chainal <chainal@qq.com>
2023-08-19 22:47:17 +0800

    set page layout to Drawer

--- app-ui/src/App.js
+++ app-ui/src/App.js
@@ -5,6 +5,7 @@ import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import ProTip from './ProTip';
 import BasicTabs from './BasicTabs'
+import ClippedDrawer from './ClippedDrawer';
 
 function Copyright() {
   return (
@@ -21,15 +22,17 @@ function Copyright() {
 
 export default function App() {
   return (
-    <Container maxWidth="lg">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          十三番機
-        </Typography>
-        <BasicTabs />
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <ClippedDrawer />
+    // <Container maxWidth="lg">
+     
+    //   <Box sx={{ my: 4 }}>
+    //     <Typography variant="h4" component="h1" gutterBottom>
+    //       十三番機
+    //     </Typography>
+    //     <BasicTabs />
+    //     <ProTip />
+    //     <Copyright />
+    //   </Box>
+    // </Container>
   );
 }
