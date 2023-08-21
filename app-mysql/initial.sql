45119d51bf5e996d2725aa8fe7b394df5bfdccfe
chainal <chainal@qq.com>
2023-08-21 18:04:25 +0800

    modify mysql time zone

--- app-mysql/initial.sql
+++ app-mysql/initial.sql
@@ -3,8 +3,15 @@ CREATE USER 'springuser'@'%' IDENTIFIED BY 'ThePassword';
 GRANT ALL ON kotoba.* TO 'springuser'@'%';
 
 USE kotoba;
-CREATE TABLE tango (
+CREATE TABLE t_tango (
     tango_id BIGINT NOT NULL AUTO_INCREMENT,
-    tango_kana VARCHAR(255),
+    word VARCHAR(255),
+    kana VARCHAR(255),
+    kana_tone INTEGER,
+    type_info VARCHAR(255),
+    meaning VARCHAR(255),
+    unit_name VARCHAR(255),
+    update_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    
     PRIMARY KEY (tango_id)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4;
